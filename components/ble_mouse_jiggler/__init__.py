cc0cdf0748aaeb55586f405e9014d11aaa2e444f
grozycki <grzegorz@php7.pl>
2025-10-10 07:26:46 +0200

    Refactor BLE mouse jiggler implementation to support multiple configurations and enhance setup process

--- components/ble_mouse_jiggler/__init__.py
+++ components/ble_mouse_jiggler/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import esp32
+from esphome.components import esp32_ble
 from esphome import automation
 from esphome.const import (
     CONF_ID,
@@ -9,87 +9,74 @@ from esphome.const import (
     CONF_INTERVAL,
 )
 
-DEPENDENCIES = ["esp32"]  # Removing esp32_ble - it might cause conflicts
-AUTO_LOAD = ["esp32"]
-CODEOWNERS = ["@esphome/core"]
+DEPENDENCIES = ["esp32_ble"]
+CODEOWNERS = ["@grozycki"]
 
-# Define custom configuration keys not available in esphome.const
 CONF_MANUFACTURER = "manufacturer"
-CONF_DISTANCE = "distance"
-CONF_MOUSE_ID = "mouse_id"
+CONF_JIGGLE_DISTANCE = "jiggle_distance"
 CONF_PIN_CODE = "pin_code"
 
-# Namespace and class
 ble_mouse_jiggler_ns = cg.esphome_ns.namespace("ble_mouse_jiggler")
 BleMouseJiggler = ble_mouse_jiggler_ns.class_("BleMouseJiggler", cg.Component)
 
-# Actions for automation - using correct base class
 StartJigglingAction = ble_mouse_jiggler_ns.class_("StartJigglingAction", automation.Action)
 StopJigglingAction = ble_mouse_jiggler_ns.class_("StopJigglingAction", automation.Action)
 JiggleOnceAction = ble_mouse_jiggler_ns.class_("JiggleOnceAction", automation.Action)
 
-# Configuration schema
-CONFIG_SCHEMA = cv.Schema(
+# Schema for a single mouse configuration
+MOUSE_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(BleMouseJiggler),
         cv.Optional(CONF_NAME, default="ESP32 Mouse Jiggler"): cv.string,
         cv.Optional(CONF_MANUFACTURER, default="ESPHome"): cv.string,
         cv.Optional(CONF_BATTERY_LEVEL, default=100): cv.int_range(min=0, max=100),
         cv.Optional(CONF_INTERVAL, default="60s"): cv.positive_time_period_milliseconds,
-        cv.Optional(CONF_DISTANCE, default=1): cv.int_range(min=1, max=10),
-        cv.Optional(CONF_MOUSE_ID, default=0): cv.int_range(min=0, max=255),
+        cv.Optional(CONF_JIGGLE_DISTANCE, default=1): cv.int_range(min=1, max=10),
         cv.Optional(CONF_PIN_CODE): cv.string,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
+# Allow a single config or a list of configs
+CONFIG_SCHEMA = cv.ensure_list(MOUSE_SCHEMA)
+
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    # This will be called for each item in the list
+    for i, conf in enumerate(config):
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
 
-    cg.add(var.set_device_name(config[CONF_NAME]))
-    cg.add(var.set_manufacturer(config[CONF_MANUFACTURER]))
-    cg.add(var.set_battery_level(config[CONF_BATTERY_LEVEL]))
-    cg.add(var.set_jiggle_interval(config[CONF_INTERVAL]))
-    cg.add(var.set_jiggle_distance(config[CONF_DISTANCE]))
-    cg.add(var.set_mouse_id(config[CONF_MOUSE_ID]))
+        cg.add(var.set_device_name(conf[CONF_NAME]))
+        cg.add(var.set_manufacturer(conf[CONF_MANUFACTURER]))
+        cg.add(var.set_battery_level(conf[CONF_BATTERY_LEVEL]))
+        cg.add(var.set_jiggle_interval(conf[CONF_INTERVAL]))
+        cg.add(var.set_jiggle_distance(conf[CONF_JIGGLE_DISTANCE]))
+        cg.add(var.set_mouse_id(i)) # Automatically assign mouse_id
 
-    # Add PIN code if provided
-    if CONF_PIN_CODE in config:
-        cg.add(var.set_pin_code(config[CONF_PIN_CODE]))
+        if CONF_PIN_CODE in conf:
+            cg.add(var.set_pin_code(conf[CONF_PIN_CODE]))
+
+        # This is the crucial part that was missing
+        cg.add_library("T-vK/ESP32-BLE-Mouse", None)
+        cg.add(var.setup())
 
 
-# Actions for ESPHome automation
 @automation.register_action(
-    "ble_mouse_jiggler.start_jiggling",
+    "ble_mouse_jiggler.start",
     StartJigglingAction,
     cv.Schema({cv.GenerateID(): cv.use_id(BleMouseJiggler)}),
 )
-async def start_jiggling_action_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    parent = await cg.get_variable(config[CONF_ID])
-    cg.add(var.set_parent(parent))
-    return var
-
-
 @automation.register_action(
-    "ble_mouse_jiggler.stop_jiggling",
+    "ble_mouse_jiggler.stop",
     StopJigglingAction,
     cv.Schema({cv.GenerateID(): cv.use_id(BleMouseJiggler)}),
 )
-async def stop_jiggling_action_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    parent = await cg.get_variable(config[CONF_ID])
-    cg.add(var.set_parent(parent))
-    return var
-
-
 @automation.register_action(
     "ble_mouse_jiggler.jiggle_once",
     JiggleOnceAction,
     cv.Schema({cv.GenerateID(): cv.use_id(BleMouseJiggler)}),
 )
-async def jiggle_once_action_to_code(config, action_id, template_arg, args):
+async def jiggler_action_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     parent = await cg.get_variable(config[CONF_ID])
     cg.add(var.set_parent(parent))
